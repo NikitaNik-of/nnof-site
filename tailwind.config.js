ef1a5536612d077e7e21c3dc79a4ba263d10f7e0
NikitaNik_of <ncayka@gmail.com>
2022-09-24 16:03:36 +0300

    font fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Igra Sans', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
