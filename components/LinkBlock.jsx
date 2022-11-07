3976312611812780b97fe4aba1d958ec22abf6a6
NikitaNik_of <ncayka@gmail.com>
2022-11-07 21:58:06 +0300

    coloring

--- components/LinkBlock.jsx
+++ components/LinkBlock.jsx
@@ -16,8 +16,8 @@ const LinkBlock = ({ playlistID, channelID, body, pic, ...props}) => {
   });
 
   const coloring = (id) => {
-    if (id == "@NikitaNik_of")return "bg-purple-300/50 dark:bg-purple-700/10"
-    if (id == "@NikitaNik_not_of")return "bg-red-300/50 dark:bg-red-700/10"
+    if (id == "@NikitaNik_of")return "bg-purple-300/30 dark:bg-purple-800/20"
+    if (id == "@NikitaNik_not_of")return "bg-red-300/30 dark:bg-red-800/20"
   } 
   
   useEffect(() => {
@@ -27,25 +27,28 @@ const LinkBlock = ({ playlistID, channelID, body, pic, ...props}) => {
   return (
     <div className="w-screen h-auto my-4">
       <Layout className={coloring(channelID) +" py-4"}>
-        <div className="flex justify-between items-center">
-          <div className="flex space-x-4 items-center">
+        <div className="flex flex-col sm:flex-row justify-between items-center">
+          <div className="flex items-center">
             <Image alt="" src={pic} height={90} width={90} className="p-2 rounded-full"/>
-            <div className="">
+            <div className="mx-2">
               <div className="text-base lg:text-2xl">{channelID}</div>
               <div className="text-[12px] lg:text-base text-gray-500">{body}</div>
             </div>
           </div>
-          <a href={"https://youtube.com/"+ channelID +"/?sub_confirmation=1"}>
-            <MainButton
-              onClick={() => {
-                console.log(vidList);
-              }}
-            >
-              Подписаться
-            </MainButton>
-          </a>
+          <div>
+            <a className={ channelID=="@NikitaNik_not_of" ? "visible" : "hidden"} href="https://www.twitch.tv/nikitanik_of/">
+              <MainButton type="violet">
+                Прямой эфир
+              </MainButton>
+            </a>
+            <a href={"https://youtube.com/"+ channelID +"/?sub_confirmation=1"}>
+              <MainButton type="red">
+                Подписаться
+              </MainButton>
+            </a>
+          </div>
         </div>
-        <div className="my-4 grid grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="my-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {isLoading ?
           vidList.map((vid) => (
             <LoadingVideoBlock key={vid.id}/>
--- components/UI/MainButton.jsx
+++ components/UI/MainButton.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
 
-const MainButton = ({disabled, children, color = 'default', className, ...props}) => {
+const MainButton = ({disabled, children, type = 'default', className, ...props}) => {
 
-  const btnStatus = (dis) => {
+  const coloring = (type, dis) => {
     if (dis) return 'bg-gray-400 dark:bg-gray-700 text-gray-500 dark:text-gray-800 cursor-not-allowed '
-    else return 'bg-gray-300 dark:bg-gray-800 text-black dark:text-white dark:hover:bg-gray-700 hover:bg-gray-200 '
-  }
-
-  const coloring = (clr) => {
-    if (clr == "default") return " dark:border-gray-600 border-gray-400 "
-    else if (clr == "red") return " dark:border-red-600 border-red-400 "
+    else if (type == "default") return " dark:border-gray-600 border-gray-400 bg-gray-300 dark:bg-gray-800 text-black dark:text-white dark:hover:bg-gray-700 hover:bg-gray-200 "
+    else if (type == "red") return " dark:border-red-600 border-red-500 bg-red-400 dark:bg-red-900/50 text-black dark:text-white dark:hover:bg-red-700/50 hover:bg-red-300 "
+    else if (type == "violet") return " dark:border-violet-600 border-violet-500 bg-violet-400 dark:bg-violet-900/50 text-black dark:text-white dark:hover:bg-violet-700/50 hover:bg-violet-300 "
   }
 
   return (
-    <button {...props} className={btnStatus(disabled) + coloring(color) + ' rounded-lg border-2 text-sm lg:text-base m-1 py-2 px-3 lg:px-5 transition-all -skew-x-6 ' + className}>
+    <button {...props} className={coloring(type, disabled) + ' rounded-lg border-2 text-[14px] lg:text-base m-1 py-2 px-3 lg:px-5 transition-all -skew-x-6 ' + className}>
         <div className='skew-x-6 flex flex-row'>
           {children}
         </div>
