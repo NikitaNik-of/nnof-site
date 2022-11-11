b8c90acc475f911eb58cb1e68e2325fc5fdaadc9
NikitaNik_of <ncayka@gmail.com>
2022-11-11 21:55:04 +0300

    some tweaks

--- components/Maintenance.jsx
+++ components/Maintenance.jsx
@@ -5,8 +5,8 @@ import Construction from '../public/images/Construction.png'
 
 const Maintenance = () => {
   return (
-    <div className='justify-center flex'>
-      <div className='z-30 flex flex-col justify-center text-center items-center h-screen bg-gray-50/30 dark:bg-gray-900/30 backdrop-blur w-11/12 lg:w-4/5'>
+    <div className='justify-center flex bg-gray-50/30 dark:bg-gray-900/30 backdrop-blur'>
+      <div className='z-30 flex flex-col justify-center text-center items-center h-screen w-11/12 lg:w-4/5'>
         <SlideUp className="flex overflow-visible" pauseFor={0}>
           <Image alt="" unoptimized src={Construction} height={100} width={100} placeholder="blur" className=" overflow-visible"/>
         </SlideUp>
@@ -16,9 +16,9 @@ const Maintenance = () => {
         </div>
         <div className="transition-all text-xl md:text-3xl md:my-2 flex space-x-2 flex-wrap justify-center">
           <SlideUp pauseFor={600}>{'Сайт '}</SlideUp>
-          <SlideUp pauseFor={700}>{'становится '}</SlideUp>
+          <SlideUp pauseFor={700}>{'скоро '}</SlideUp>
           <SlideUp pauseFor={800} className=" bg-gradient-to-br from-yellow-500 via-amber-500 to-orange-500 text-transparent bg-clip-text ">
-            лучше
+            откроется!
           </SlideUp>
         </div>
         <SlideUp pauseFor={1000} className="mt-12 md:mt-24 text-[14px] md:text-xl">{'Прямо сейчас сайт меняется и поэтому требует уединения.'}</SlideUp>
