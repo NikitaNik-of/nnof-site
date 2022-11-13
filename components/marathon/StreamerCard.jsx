6f4d55b4730f1cb08a145130a6f1dd8d5a631714
NikitaNik_of <ncayka@gmail.com>
2022-11-14 00:20:00 +0300

    status before/after template

--- components/marathon/StreamerCard.jsx
+++ components/marathon/StreamerCard.jsx
@@ -8,8 +8,8 @@ const StreamerCard = ({ streamerInfo, times }) => {
   }
 
   return (
-    <div className={'flex flex-row min-w-full sm:min-w-0 sm:flex-col my-4 justify-center items-center bg-gray-200/50 dark:bg-gray-800/50 p-6 rounded-lg m-2 mx-4 hover:-translate-y-1 hover:scale-[1.01] transition-all hover:bg-gray-300 hover:dark:bg-gray-700/50 hover:shadow-2xl ' + streamerInfo.hover}>
-      <div className={'w-20 md:w-32 lg:w-40 p-px bg-gradient-to-br rounded-lg ' + streamerInfo.color}>
+    <a href={"https://twitch.tv" + streamerInfo.name} className={'flex flex-row min-w-full sm:min-w-0 sm:flex-col my-4 justify-center items-center bg-gray-200/50 dark:bg-gray-800/50 p-6 rounded-lg m-2 mx-4 hover:-translate-y-1 hover:scale-[1.01] transition-all hover:bg-gray-300 hover:dark:bg-gray-700/50 hover:shadow-2xl ' + streamerInfo.hover}>
+      <div className={'w-20 md:w-32 lg:w-40 p-px bg-gradient-to-bl rounded-lg ' + streamerInfo.color}>
         <div className='bg-gray-200 dark:bg-gray-900 rounded-lg'>
           <div className={'rounded-lg p-2 md:p-4 lg:p-6 ' + streamerInfo.mainColor}>
             <Image className='object-cover object-top aspect-square' alt="" loader={skinLoader} src={streamerInfo.MCName} width={393} height={849}/>
@@ -20,11 +20,11 @@ const StreamerCard = ({ streamerInfo, times }) => {
         <div className='text-xl lg:text-2xl'>
           {streamerInfo.name}
         </div>
-        <div className='sm:mt-1 text-base lg:text-lg'>
+        <div className='sm:mt-1 text-base lg:text-lg text-gray-500'>
           {times[0] + " - " + times[1]}
         </div>
       </div>
-    </div>
+    </a>
   )
 }
 
--- pages/marathon.js
+++ pages/marathon.js
@@ -4,27 +4,30 @@ import { useState } from "react";
 import NavBar from "../components/NavBar";
 import LinkButton from "../components/UI/LinkButton";
 import links from "../api/links.json"
-import TitleHead from "../components/TitleHead";
+import BeforeMara from '../components/marathon/BeforeMara';
 import maraInfo from "../api/streamers.json"
-import StreamerCard from '../components/marathon/StreamerCard';
 
 export default function Marathon() {
     const [linkBtn, setLink] = useState(links.linkBtn)
-    const [curMara, setCurMara] = useState(maraInfo.curMarathon - 4)
-    const dateL = new Date(maraInfo.marathons[curMara].date + maraInfo.marathons[curMara].times[0] + ":00+03")
+
+    const stateMar = () => {
+        // 0=before, 1=playing, 2=after
+        let now = new Date()
+        let start = new Date(maraInfo.marathons[maraInfo.curMarathon - 4].date + maraInfo.marathons[maraInfo.curMarathon - 4].times[0])
+        let end = new Date(maraInfo.marathons[maraInfo.curMarathon - 4].date + maraInfo.marathons[maraInfo.curMarathon - 4].times[maraInfo.marathons[maraInfo.curMarathon - 4].streamCount - 1])
+        let diffStart = start.getTime() - now.getTime()
+        let diffEnd = end.getTime() - now.getTime()
+        console.log(start.getTime(), diffStart)
+        console.log(end.getTime(), diffEnd)
+        if (diffStart > 0) return 0
+        else if (diffEnd > 0) return 1
+        else return 2
+    } 
 
     return(
     <div className=" font-igraSans">
         <NavBar/>
-        <Layout className='my-16'>
-            <Head><title>Эстафета стримеров</title></Head>
-            <TitleHead title="Эстафета стримеров" subtitle="Генератор часов контента для зрителей" hidtitle='"А кому мне рейд кидать?" - кто-то из участников эстафеты'/>
-        </Layout>
-        <div className='flex justify-center p-4 flex-wrap lg:mx-8'>
-            {maraInfo.marathons[curMara].streamersID.map((streamer, key) => (
-                <StreamerCard key={key} streamerInfo={maraInfo.profiles[streamer - 1]} times={[maraInfo.marathons[curMara].times[key], maraInfo.marathons[curMara].times[key + 1]]}/>
-            ))}
-        </div>
+        <BeforeMara isBefore={stateMar()} maraInfo={maraInfo} isTimer=""/>
     </div>
     )
 }
