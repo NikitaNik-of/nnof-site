2e13ba3c003ac7ca72032b017a0ebfeef4e41851
NikitaNik_of <ncayka@gmail.com>
2022-11-17 11:53:15 +0300

    +date

--- components/marathon/LastMaraList.jsx
+++ components/marathon/LastMaraList.jsx
@@ -18,7 +18,7 @@ const LastMaraList = ({ streamers, marathons, curMar }) => {
     <div className="bg-gray-300/50 dark:bg-gray-800/50 my-16 -skew-y-3 py-4 md:p-4 text-center">
       <div className=" text-lg md:text-2xl mt-4 mb-2 skew-y-3">Прошедшие эстафеты</div>
       <div className=" text-sm md:text-base my-4 skew-y-3 flex flex-col justify-center items-center w-11/12 mx-auto">
-        <div className=" text-gray-600 dark:text-gray-600">Все анонсы публикуются в <text className="text-[#5865F2]">Discord</text> и в <text className="text-[#0088cc]">Telegram</text></div>
+        <div className=" text-gray-600 dark:text-gray-500">Все анонсы публикуются в <text className="text-[#5865F2]">Discord</text> и в <text className="text-[#0088cc]">Telegram</text></div>
         <div className="flex flex-wrap justify-around">
           <LinkButton className={links.linkBtn[5].classBtn} href={links.linkBtn[5].href}>{links.linkBtn[5].name}</LinkButton>
           <LinkButton className={links.linkBtn[2].classBtn} href={links.linkBtn[2].href}>{links.linkBtn[2].name}</LinkButton>
@@ -29,7 +29,8 @@ const LastMaraList = ({ streamers, marathons, curMar }) => {
         <div className=" grid grid-cols-1 md:grid-cols-2 skew-y-3">
           {marathons.map((mar, key) => (key < curMar
             ? <div className="bg-gray-400/20 dark:bg-gray-700/30 px-3 pb-3 my-2 md:m-2 rounded-3xl overflow-hidden">
-                <div className="my-3 text-base md:text-xl">{"Анонс эстафеты #" + (curMar - key)}</div>
+                <div className="mt-3 text-base md:text-xl">{"Анонс эстафеты #" + (curMar - key)}</div>
+                <div className="mb-3 text-sm md:text-base text-gray-600 dark:text-gray-500">{mar.dateStr}</div>
                 <Image className=" rounded-xl" src={maraPics[curMar - key - 1]} unoptimized/>
               </div>
             : null))}
